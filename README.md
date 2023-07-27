eb0824cfbc425497a09e7374fc1253b9bdf8c4e0
keith <keithneo00@gmail.com>
2023-07-27 23:48:58 +0800

    Made some changes

---
+++ README.md
@@ -0,0 +1,4 @@
+# CDC crawler bot
+## update:
+User to input what the bot should look for 
+User to login and go to page 
--- bot.py
+++ bot.py
@@ -80,7 +80,7 @@ class Bot:
         WebDriverWait(self.driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[name^='a-'][src^='https://www.google.com/recaptcha/api2/anchor?']")))
         WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='recaptcha-anchor']"))).click()
         time.sleep(2)
-        self.driver.find_element(By.XPATH, '//*[@id="BTNSERVICE2"]').click()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#BTNSERVICE2"))).click()
 
     def start(self, **data):
         time.sleep(3)
